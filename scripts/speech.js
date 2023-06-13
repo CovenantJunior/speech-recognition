74ba70faab5d25c9a443e0d0c8cb6a528b7dfed0
Covenant Junior <covenant@gmail.com>
2023-06-13 18:55:19 +0100

    STT Synthesis

--- scripts/speech.js
+++ scripts/speech.js
@@ -1,10 +1,33 @@
+// TTS Synthesis
 const play = $('#play');
 const speak = $('#speak');
 const text = $('#text');
+const output = $('#output');
 
 const synthesis = window.speechSynthesis;
 
 play.on('click', () => {
+    // Utterance
     let _u = new SpeechSynthesisUtterance(text.val());
     synthesis.speak(_u);
-})
+});
+
+
+// STT Synthesis
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+const _r = new SpeechRecognition();
+
+speak.on('click', () => {
+    // Recognition
+    _r.start();
+});
+
+_r.onresult = (e) => {
+    let transcription = e.results[0][0].transcript;
+    output.text(transcription);
+    text.val(transcription);
+};
+
+_r.onerror = (e) => {
+    output.text("Error: " + e.error);
+};
